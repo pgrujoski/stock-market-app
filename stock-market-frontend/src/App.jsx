9b65190c61a68e7579b6b9ae362a517b8bf1db5c
pgrujoski <petar.grujoski@gmail.com>
2024-06-10 20:39:48 +0200

    initial frontend App.jsx

--- stock-market-frontend/src/App.jsx
+++ stock-market-frontend/src/App.jsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Stocks = ({ addToWatchlist }) => {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:8008/api/stocks")
+      .then((res) => res.json())
+      .then((data) => setStocks(data))
+      .catch((err) => console.error("Error fetching data", err));
+  }, []);
+  console.log(setStocks, "StocksData");
+
+  const getRandomColor = () => {
+    const colors = ["#FF0000", "#00FF00"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Stock Market MERN app</h1>
+      <h2>Stocks</h2>
+      <ul>
+        {stocks.map((stock) => {
+          <li key={stock.symbol}>
+            {stock.company} ({stock.symol}) -
+            <span style={{ color: getRandomColor() }}>
+              {" "}
+              ${stock.initial_price}
+            </span>
+            <button onClick={() => addToWatchlist(stock)}>
+              Add to my Watchlist
+            </button>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+const Watchlist = ({ watchlist }) => {
+  const getRandomColor = () => {
+    const colors = ["#FF0000", "#00FF00"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+  return (
+    <div className="App">
+      <h1>Stock Market MERN app</h1>
+      <h2>MY Watchlist</h2>
+      <ul>
+        {watchlist.map((stock) => (
+          <li key={stock.symbol}>
+            {stock.company} ({stock.symbol}) -
+            <span style={{ color: getRandomColor() }}>
+              {" "}
+              ${stock.initial_price}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+function App() {
+  const [watchlist, setWatchlist] = useState([]);
+  const addToWatchlist = (stock) => {
+    fetch("http://localhost:8008/api/watchlist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(stock),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message);
+        setWatchlist([...watchlist, stock]);
+      })
+      .catch((error) => {
+        console.log("Error adding to watchlist", error);
+      });
+  };
+
+  return (
+    <Router>
+      <nav>
+        <NavLink to="/stocks">Stocks</NavLink>
+        <NavLink to="/watchlist">Watchlist</NavLink>
+        <Routes>
+          <Route
+            path="/stocks"
+            element={<Stocks addToWatchlist={addToWatchlist} />}
+          />
+          <Route
+            path="/watchlist"
+            element={<Watchlist watchlist={watchlist} />}
+          />
+        </Routes>
+      </nav>
+    </Router>
+  );
 }
 
-export default App
+export default App;
